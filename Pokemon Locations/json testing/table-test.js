6a5c7e371e86dda36ff3e56d1d1b7463b37566c7
Altissimo1 <100002822+Altissimo1@users.noreply.github.com>
2022-02-24 09:02:44 -0500

    Create table-test.js

---
+++ Pokemon Locations/json testing/table-test.js
@@ -0,0 +1,28 @@
+function updateImage() {
+	
+	fetch('table-test.json')
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			appendIcons(data);
+		})
+		.catch(function (err) {
+			console.log('error: ' + err);
+		});
+		
+		function appendIcons(data) {
+      var container = document.getElementById("table-displays");
+      for (var i = 0; i < data.length; i++) {
+        var full = data[i].full;
+        for (var j = 0; j < full.length; j++) {
+          container.innerText += full[j].heading;
+        }
+      }
+      
+      
+      
+    }
+	
+	
+}
