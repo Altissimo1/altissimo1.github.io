d57db03fca97d95f107fc928d1ab9bb440ee2962
Altissimo1 <100002822+Altissimo1@users.noreply.github.com>
2023-02-07 12:46:59 -0500

    Update qr-slides.js

--- Side Series/Mystery Dungeon/Super Mystery Dungeon/javascript/qr-slides.js
+++ Side Series/Mystery Dungeon/Super Mystery Dungeon/javascript/qr-slides.js
@@ -3,7 +3,7 @@ $(function() {
 	var usCodes = [];
 	var jpCodes = [];
 	var euCodes = [];
-	$.getJSON("json/qr-codes.json", function(data) {
+	$.getJSON("qr-codes.json", function(data) {
 		$.each(data, function() {
 			if (this.region == "US")
 				usCodes.push(this);
@@ -13,6 +13,34 @@ $(function() {
 				jpCodes.push(this);
 		});
 	});
+	
+	$(".checkbox").change(function() {
+		addRemoveItem(this);
+	});
+	
+	$(".allcheck").change(function() {
+		if (this.checked) {
+			$("." + this.id + " div input").each(function() {
+				$(this).prop("checked", true);
+				addRemoveItem(this);
+			});
+		}
+		else
+			$("." + this.id + " div input").each(function() {
+				$(this).prop("checked", false);
+				addRemoveItem(this);
+			});
+	});
+	
+	function addRemoveItem(checkItem) {
+		if (checkItem.checked) {
+			var textDisplay = "<span name='" + checkItem.id + "'><br>" + checkItem.value + "</span>";
+			$("#textlist").html($("#textlist").html() + textDisplay);
+		}
+		else {
+			$("." + checkItem.id).remove();
+		}
+	}
 	
 	$("#generate").click(function() {
 		var currentRegion = "";
@@ -25,7 +53,7 @@ $(function() {
 		
 		var display = true;
 		
-		$(".container input:checked").each(function() {
+		$("#textlist span").each(function() {
 			// Create an image item
 			if (!display)
 				innerText += "<div style='margin:auto;width:50%;display:none;'>";
@@ -34,15 +62,14 @@ $(function() {
 				display = false;
 			}
 			
-			innerText += "<span><strong>" + $(this)[0].value + "</strong></span><br>";
+			innerText += "<span><strong>" + this.textContent + "</strong></span><br>";
 			var jsonObject;
-			console.log(currentRegion);
 			if (currentRegion == "us")
-				jsonObject = usCodes[parseInt($(this).attr("id")) - 1];
+				jsonObject = usCodes[parseInt($(this).attr("name")) - 1];
 			else if (currentRegion == "eu")
-				jsonObject = euCodes[parseInt($(this).attr("id")) - 1];
+				jsonObject = euCodes[parseInt($(this).attr("name")) - 1];
 			else
-				jsonObject = jpCodes[parseInt($(this).attr("id")) - 1];
+				jsonObject = jpCodes[parseInt($(this).attr("name")) - 1];
 			innerText += '<img src="' + jsonObject.src + '" alt="' + jsonObject.alt + '"></div>';
 		});
 		
