2625da4ebe46d2ad1321ba94e471f029ba728e36
Altissimo1 <100002822+Altissimo1@users.noreply.github.com>
2026-06-15 16:50:32 -0400

    Daily update

---
+++ Main-Series/javascript/ORAS/mossdeep-city.js
@@ -0,0 +1,225 @@
+window.ORAS_DATA = {
+  "name": "Mossdeep City",
+  "encounters": {
+    "surfing": [
+      {
+        "rate": 50,
+        "slot": 0,
+        "options": [
+          {
+            "level": "25",
+            "name": "Tentacool",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 30,
+        "slot": 1,
+        "options": [
+          {
+            "level": "25",
+            "name": "Pelipper",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 15,
+        "slot": 2,
+        "options": [
+          {
+            "level": "30",
+            "name": "Tentacruel",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 4,
+        "slot": 3,
+        "options": [
+          {
+            "level": "30",
+            "name": "Pelipper",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 1,
+        "slot": 4,
+        "options": [
+          {
+            "level": "35",
+            "name": "Pelipper",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      }
+    ],
+    "fishing": {
+      "old": [
+        {
+          "rate": 60,
+          "slot": 0,
+          "options": [
+            {
+              "level": "10",
+              "name": "Magikarp",
+              "games": [
+                "Omega Ruby",
+                "Alpha Sapphire"
+              ],
+              "conditions": []
+            }
+          ]
+        },
+        {
+          "rate": 35,
+          "slot": 1,
+          "options": [
+            {
+              "level": "5",
+              "name": "Tentacool",
+              "games": [
+                "Omega Ruby",
+                "Alpha Sapphire"
+              ],
+              "conditions": []
+            }
+          ]
+        },
+        {
+          "rate": 5,
+          "slot": 2,
+          "options": [
+            {
+              "level": "15",
+              "name": "Magikarp",
+              "games": [
+                "Omega Ruby",
+                "Alpha Sapphire"
+              ],
+              "conditions": []
+            }
+          ]
+        }
+      ],
+      "good": [
+        {
+          "rate": 60,
+          "slot": 0,
+          "options": [
+            {
+              "level": "25",
+              "name": "Magikarp",
+              "games": [
+                "Omega Ruby",
+                "Alpha Sapphire"
+              ],
+              "conditions": []
+            }
+          ]
+        },
+        {
+          "rate": 35,
+          "slot": 1,
+          "options": [
+            {
+              "level": "25",
+              "name": "Tentacool",
+              "games": [
+                "Omega Ruby",
+                "Alpha Sapphire"
+              ],
+              "conditions": []
+            }
+          ]
+        },
+        {
+          "rate": 5,
+          "slot": 2,
+          "options": [
+            {
+              "level": "25",
+              "name": "Wailmer",
+              "games": [
+                "Omega Ruby",
+                "Alpha Sapphire"
+              ],
+              "conditions": []
+            }
+          ]
+        }
+      ],
+      "super": [
+        {
+          "rate": 60,
+          "slot": 0,
+          "options": [
+            {
+              "level": "25",
+              "name": "Wailmer",
+              "games": [
+                "Omega Ruby",
+                "Alpha Sapphire"
+              ],
+              "conditions": []
+            }
+          ]
+        },
+        {
+          "rate": 35,
+          "slot": 1,
+          "options": [
+            {
+              "level": "30",
+              "name": "Wailmer",
+              "games": [
+                "Omega Ruby",
+                "Alpha Sapphire"
+              ],
+              "conditions": []
+            }
+          ]
+        },
+        {
+          "rate": 5,
+          "slot": 2,
+          "options": [
+            {
+              "level": "40",
+              "name": "Wailmer",
+              "games": [
+                "Omega Ruby",
+                "Alpha Sapphire"
+              ],
+              "conditions": []
+            }
+          ]
+        }
+      ]
+    }
+  }
+};
