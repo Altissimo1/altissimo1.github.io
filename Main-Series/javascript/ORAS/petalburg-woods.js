a2c4887997b7ed068c4cbdc5c7b8578953dbbd87
Altissimo1 <100002822+Altissimo1@users.noreply.github.com>
2026-06-23 16:48:16 -0400

    Daily update

---
+++ Main-Series/javascript/ORAS/petalburg-woods.js
@@ -0,0 +1,187 @@
+window.ORAS_DATA = {
+  "name": "Petalburg Woods",
+  "encounters": {
+    "walking": [
+      {
+        "rate": 10,
+        "slot": 0,
+        "options": [
+          {
+            "level": "4",
+            "name": "Zigzagoon",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 1,
+        "options": [
+          {
+            "level": "5",
+            "name": "Zigzagoon",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 2,
+        "options": [
+          {
+            "level": "6",
+            "name": "Zigzagoon",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 3,
+        "options": [
+          {
+            "level": "4",
+            "name": "Wurmple",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 4,
+        "options": [
+          {
+            "level": "5",
+            "name": "Wurmple",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 5,
+        "options": [
+          {
+            "level": "6",
+            "name": "Wurmple",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 6,
+        "options": [
+          {
+            "level": "6",
+            "name": "Silcoon",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 7,
+        "options": [
+          {
+            "level": "6",
+            "name": "Cascoon",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 8,
+        "options": [
+          {
+            "level": "5",
+            "name": "Shroomish",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 5,
+        "slot": 9,
+        "options": [
+          {
+            "level": "5",
+            "name": "Taillow",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 4,
+        "slot": 10,
+        "options": [
+          {
+            "level": "5",
+            "name": "Slakoth",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 1,
+        "slot": 11,
+        "options": [
+          {
+            "level": "5",
+            "name": "Slakoth",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      }
+    ]
+  }
+};
---
+++ Main-Series/javascript/RSE/petalburg-woods.js
@@ -0,0 +1,213 @@
+window.RSE_DATA = {
+  "name": "petalburg_woods",
+  "encounters": {
+    "walking": [
+      {
+        "rate": 20,
+        "slot": 0,
+        "options": [
+          {
+            "level": "5",
+            "name": "Zigzagoon",
+            "games": [
+              "Ruby",
+              "Sapphire"
+            ],
+            "conditions": []
+          },
+          {
+            "level": "5",
+            "name": "Poochyena",
+            "games": [
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 20,
+        "slot": 1,
+        "options": [
+          {
+            "level": "5",
+            "name": "Wurmple",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 2,
+        "options": [
+          {
+            "level": "5",
+            "name": "Shroomish",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 3,
+        "options": [
+          {
+            "level": "6",
+            "name": "Zigzagoon",
+            "games": [
+              "Ruby",
+              "Sapphire"
+            ],
+            "conditions": []
+          },
+          {
+            "level": "6",
+            "name": "Poochyena",
+            "games": [
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 4,
+        "options": [
+          {
+            "level": "5",
+            "name": "Silcoon",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 5,
+        "options": [
+          {
+            "level": "5",
+            "name": "Cascoon",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 5,
+        "slot": 6,
+        "options": [
+          {
+            "level": "6",
+            "name": "Wurmple",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 5,
+        "slot": 7,
+        "options": [
+          {
+            "level": "6",
+            "name": "Shroomish",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 4,
+        "slot": 8,
+        "options": [
+          {
+            "level": "5",
+            "name": "Taillow",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 4,
+        "slot": 9,
+        "options": [
+          {
+            "level": "5",
+            "name": "Slakoth",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 1,
+        "slot": 10,
+        "options": [
+          {
+            "level": "6",
+            "name": "Taillow",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 1,
+        "slot": 11,
+        "options": [
+          {
+            "level": "6",
+            "name": "Slakoth",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      }
+    ]
+  }
+};
