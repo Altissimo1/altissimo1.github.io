acd9e0bb54f3474d22b902ea0a02d418e3d45b76
Altissimo1 <100002822+Altissimo1@users.noreply.github.com>
2026-06-24 11:10:43 -0400

    Added some Guardian Signs info

---
+++ Main-Series/javascript/ORAS/rusturf-tunnel.js
@@ -0,0 +1,264 @@
+window.ORAS_DATA = {
+  "name": "Rusturf Tunnel",
+  "encounters": {
+    "walking": [
+      {
+        "rate": 10,
+        "slot": 0,
+        "options": [
+          {
+            "level": "7",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 1,
+        "options": [
+          {
+            "level": "7",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 2,
+        "options": [
+          {
+            "level": "8",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 3,
+        "options": [
+          {
+            "level": "8",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 4,
+        "options": [
+          {
+            "level": "8",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 5,
+        "options": [
+          {
+            "level": "9",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 6,
+        "options": [
+          {
+            "level": "9",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 7,
+        "options": [
+          {
+            "level": "9",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 8,
+        "options": [
+          {
+            "level": "10",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 5,
+        "slot": 9,
+        "options": [
+          {
+            "level": "10",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 4,
+        "slot": 10,
+        "options": [
+          {
+            "level": "10",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 1,
+        "slot": 11,
+        "options": [
+          {
+            "level": "10",
+            "name": "Whismur",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      }
+    ],
+    "rock_smash": [
+      {
+        "rate": 50,
+        "slot": 0,
+        "options": [
+          {
+            "level": "14",
+            "name": "Geodude",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 30,
+        "slot": 1,
+        "options": [
+          {
+            "level": "15",
+            "name": "Geodude",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 15,
+        "slot": 2,
+        "options": [
+          {
+            "level": "16",
+            "name": "Geodude",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 4,
+        "slot": 3,
+        "options": [
+          {
+            "level": "17",
+            "name": "Geodude",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 1,
+        "slot": 4,
+        "options": [
+          {
+            "level": "17",
+            "name": "Geodude",
+            "games": [
+              "Omega Ruby",
+              "Alpha Sapphire"
+            ],
+            "conditions": []
+          }
+        ]
+      }
+    ]
+  }
+};
---
+++ Main-Series/javascript/RSE/rusturf-tunnel.js
@@ -0,0 +1,199 @@
+window.RSE_DATA = {
+  "name": "rusturf_tunnel",
+  "encounters": {
+    "walking": [
+      {
+        "rate": 20,
+        "slot": 0,
+        "options": [
+          {
+            "level": "6",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 20,
+        "slot": 1,
+        "options": [
+          {
+            "level": "7",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 2,
+        "options": [
+          {
+            "level": "6",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 3,
+        "options": [
+          {
+            "level": "6",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 4,
+        "options": [
+          {
+            "level": "7",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 10,
+        "slot": 5,
+        "options": [
+          {
+            "level": "7",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 5,
+        "slot": 6,
+        "options": [
+          {
+            "level": "5",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 5,
+        "slot": 7,
+        "options": [
+          {
+            "level": "8",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 4,
+        "slot": 8,
+        "options": [
+          {
+            "level": "5",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 4,
+        "slot": 9,
+        "options": [
+          {
+            "level": "8",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 1,
+        "slot": 10,
+        "options": [
+          {
+            "level": "5",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      },
+      {
+        "rate": 1,
+        "slot": 11,
+        "options": [
+          {
+            "level": "8",
+            "name": "Whismur",
+            "games": [
+              "Ruby",
+              "Sapphire",
+              "Emerald"
+            ],
+            "conditions": []
+          }
+        ]
+      }
+    ]
+  }
+};
